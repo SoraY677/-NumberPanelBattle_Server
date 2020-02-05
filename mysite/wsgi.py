32c67d54d540079622838438079965821baa75c0
SoraY677 <SoraY677@outlook.com>
2020-02-05 15:34:23 +0900

    動かねぇ！

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -12,7 +12,4 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-#application = Cling(get_wsgi_application())
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
